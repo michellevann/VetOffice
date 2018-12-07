245d9f1ec723c3ff339bc32b34bd142f9b62258c
Michelle Vann <michelle@dougvann.com>
2018-12-06 21:35:18 -0500

    Add project files.

---
+++ VetOffice.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VetOffice.WebMVC.Startup))]
+namespace VetOffice.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
