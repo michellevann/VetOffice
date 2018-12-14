7acb75a3be84b786f34eec1f957db05f126eff14
Michelle Vann <michelle@dougvann.com>
2018-12-14 16:15:49 -0500

    Updated icon bar

--- VetOffice.Models/AppointmentEdit.cs
+++ VetOffice.Models/AppointmentEdit.cs
@@ -13,7 +13,8 @@ namespace VetOffice.Models
     {
         [Key]
         public int AppointmentId { get; set; }
-
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM-dd-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime NextAppt { get; set; }
         public VisitReason ReasonForVisit { get; set; }
         public string ApptTime { get; set; }
